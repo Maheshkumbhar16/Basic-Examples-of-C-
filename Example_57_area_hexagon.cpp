ad940b04d236c515e2c30c8dcdfca0fd1c5f215d
Mahesh Kumbhar <93268828+Maheshkumbhar16@users.noreply.github.com>
2021-12-06 19:53:27 +0530

    area of a hexagon
    Write a program in C++ to print the area of a hexagon

---
+++ Example_57_area_hexagon.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+    int a=6;
+    cout<< "Side of the hexagon : "<<a<<endl;
+    cout<< "Area of the hexagon : "<<(3*a*a*sqrt(3))/2;
+return 0;
+}
