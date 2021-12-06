7c55e54e3c239181682ff1e974e80ad68c9a0bae
Mahesh Kumbhar <93268828+Maheshkumbhar16@users.noreply.github.com>
2021-12-06 19:15:06 +0530

    Size of fundamental data types
    Write a program in C++ to find Size of fundamental data types

---
+++ Example_03_size_of_datatypes.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "The size of char is :" <<sizeof(char) <<"bytes" <<endl;
+    cout << "The size of short is :" <<sizeof(short) <<"bytes" <<endl;
+    cout << "The size of int is :" <<sizeof(int) <<"bytes" <<endl;
+    cout << "The size of long is :" <<sizeof(long) <<"bytes" <<endl;
+    cout << "The size of long long is :" <<sizeof(long long) <<"bytes"<<endl;
+    cout << "The size of float is :" <<sizeof(float) <<"bytes"<<endl;
+    cout << "The size of double is :" <<sizeof(double) <<"bytes"<<endl;
+    cout << "The size of long double is :" <<sizeof(long double) <<"bytes"<<endl;
+    cout << "The size of bool is :" <<sizeof(bool) <<"bytes"<<endl;
+
+return 0;
+}
