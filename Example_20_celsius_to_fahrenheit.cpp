bd99f7db3af48519926aa5d64705d83db3bf3791
Mahesh Kumbhar <93268828+Maheshkumbhar16@users.noreply.github.com>
2021-12-06 19:31:07 +0530

    convert temperature in Celsius to Fahrenheit
    Write a program in C++ to convert temperature in Celsius to Fahrenheit

---
+++ Example_20_celsius_to_fahrenheit.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include<cmath>
+using namespace std;
+int main(){
+//formula is F=C*1.8+32
+int c=35;
+double f;
+f=c*1.8+32;
+cout << "Temp in Celsius is : "<<c<<endl;
+cout << "Temp in Fahrenheit is : "<<f;
+return 0;
+}
