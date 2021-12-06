ab2de0a5eaaa8c50a71e045e011c5f87d2be438c
Mahesh Kumbhar <93268828+Maheshkumbhar16@users.noreply.github.com>
2021-12-06 19:49:53 +0530

    accepts the user's first and last name
    Write a language program in C++ which accepts the user's first and last name and print them in reverse order with a space between them

---
+++ Example_42_user_name.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string first_name, last_name;
+    cout<< "Enter your firstname here : ";
+    cin>>first_name;
+    cout<< "Enter your lastname here : ";
+    cin>>last_name;
+    cout<< "Name in reverse is : "<<last_name<< " "<<first_name;
+return 0;
+}
