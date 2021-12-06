675b8990f062fa563c056bea6e8309cc3cc94f56
Mahesh Kumbhar <93268828+Maheshkumbhar16@users.noreply.github.com>
2021-12-06 19:20:15 +0530

    Check whether the primitive values crossing limits
    Write a program in C++ to check whether the primitive values crossing the limits or not

---
+++ Example_06_primitive_values_limits.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char gender='F';
+    bool married = true;
+    int sons = 2;
+    int year_appointment=2009;
+    long salary=1500000;
+    float height = 79.48;
+    float gpa=4.69;
+    long salary_drawn=12047235;
+    long balance=995324987;
+    cout<<"The Gender is : "<<gender<<endl;
+    cout<<"Is she married? : "<<married<<endl;
+    cout<<"Number of sons she has : "<<sons<<endl;
+    cout<<"Year of her appointment : "<<year_appointment<<endl;
+    cout<<"Salary for a year : "<<salary<<endl;
+    cout<<"Height is : "<<height<<endl;
+    cout<<"GPA is : "<<gpa<<endl;
+    cout<<"Salary drawn upto : "<<salary_drawn<<endl;
+    cout<<"Balance till : "<<balance;
+return 0;
+}
