93f5e4d7bd7fc8495a6f9ebeddc30b3fc34a5b50
Mahesh Kumbhar <93268828+Maheshkumbhar16@users.noreply.github.com>
2021-12-06 19:51:37 +0530

    Convert centimeter into meter and kilometer
    Write a program in C++ to enter length in centimeter and convert it into meter and kilometer

---
+++ Example_50_cm_meter_km.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    double num;
+    cout<< "Enter length in centimeter : ";
+    cin>>num;
+    cout<< "Length in centimeter is : "<<num<<endl;
+    cout<< "Length in meter is : "<<num/100<<endl;
+    cout<< "Length in kilometer is : "<<num/100000;
+return 0;
+}
