18489bb20bc91d12d5a2a1c8272c0179bf752db1
Mahesh Kumbhar <93268828+Maheshkumbhar16@users.noreply.github.com>
2021-12-06 19:21:40 +0530

    Various arithmetic operations using mixed datatype
    Write a program in C++ to display various type or arithmetic operation using mixed data type

---
+++ Example_07_mixed_datatypes.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x=5, y=7;
+    float a=3.7, b=8.0;
+    cout << x+y <<endl;
+    cout << a+b <<endl;
+    cout << x+b <<endl;
+    cout << x-y <<endl;
+    cout << a-b <<endl;
+    cout << x-b <<endl;
+    cout << x*y <<endl;
+    cout << a*b <<endl;
+    cout << x*b <<endl;
+    cout << x/y <<endl;
+    cout << a/b <<endl;
+    cout << x/b <<endl;
+return 0;
+}
