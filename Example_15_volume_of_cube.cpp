fa956167e6e9ea865954ff69c4626cb65241da9a
Mahesh Kumbhar <93268828+Maheshkumbhar16@users.noreply.github.com>
2021-12-06 19:28:36 +0530

    volume of a cube
    Write a program in C++ to calculate the volume of a cube

---
+++ Example_15_volume_of_cube.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    int r=5,vol;
+    vol=r*r*r;
+    cout<<"Radius of cube is : "<<r<<endl;
+    cout<<"Volume of cube is : "<<vol;
+return 0;
+}
