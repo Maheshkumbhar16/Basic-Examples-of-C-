02356c730239b9271b6a1a9e1b6907b96ac28e18
Mahesh Kumbhar <93268828+Maheshkumbhar16@users.noreply.github.com>
2021-12-06 19:44:25 +0530

    divide two numbers
    Write a program in C++ to divide two numbers and print on the screen

---
+++ Example_33_divide_two_numbers.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int num1=30, num2=10;
+    cout<< "Enter numbers are : "<<endl<<num1<<endl<<num2<<endl;
+    cout<< "Quotient of "<<num1<< " and "<<num2<< " is : "<<num1/num2;
+return 0;
+}
