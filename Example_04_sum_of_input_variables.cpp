fabecd7480be170f42b49a19f65f06a74c6180ca
Mahesh Kumbhar <93268828+Maheshkumbhar16@users.noreply.github.com>
2021-12-06 19:16:11 +0530

    Sum of two numbers
    Write a program in C++ to print the sum of two numbers using variables

---
+++ Example_04_sum_of_input_variables.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x,y;
+    cout << "Enter value of X : ";
+    cin >> x;
+    cout << "Value of X is "<<x <<endl;
+    cout << "Enter the value of Y : ";
+    cin >> y;
+    cout << "Value of Y is " <<y <<endl;
+    cout << "Sum of X and Y is " << x+y;
+return 0;
+}
