d597318355150c9b226a8ebbc4d844874ffff597
Mahesh Kumbhar <93268828+Maheshkumbhar16@users.noreply.github.com>
2021-12-06 19:46:57 +0530

    area and perimeter of a rectangle
    Write a program in C++ to print the area and perimeter of a rectangle

---
+++ Example_40_area_perimeter_reactangle.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int  main(){
+    double leng=8.5, brdth=5.6;
+    cout<< "Width of the rectangle is : "<<leng<<endl;
+    cout<< "Height of the rectangle is : "<<brdth<<endl;
+    cout<< "Area of the rectangle is : "<<leng*brdth<<endl;
+    cout<< "Perimeter of the rectangle is : "<<2*(leng+brdth);
+return 0;
+}
