05eeb7eea43441475df11f4af0cafad6120fcf0b
Mahesh Kumbhar <93268828+Maheshkumbhar16@users.noreply.github.com>
2021-12-06 19:36:29 +0530

    convert temperature in Celsius to Kelvin
    Write a program in C++ to convert temperature in Celsius to Kelvin

---
+++ Example_27_celsius_to_kelvin.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include<cmath>
+using namespace std;
+int main(){
+//formula is k=c+273.15
+double c=26.85, k;
+k=c+273.15;
+cout << "Temp in Celsius is : "<<c<<endl;
+cout << "Temp in Kelvin is : "<<k;
+return 0;
+}
