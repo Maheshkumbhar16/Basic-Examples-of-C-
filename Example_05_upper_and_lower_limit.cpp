1205b6ebfdcb772a8c3d7dfb1dd76cb4741b5050
Mahesh Kumbhar <93268828+Maheshkumbhar16@users.noreply.github.com>
2021-12-06 19:18:56 +0530

    Check the upper and lower limits of integer
    Write a program in C++ to check the upper and lower limits of integer

---
+++ Example_05_upper_and_lower_limit.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout << "Maximum limit of int data type : "<<INT_MAX<<endl;
+    cout << "Minimum limit of int data type : "<<INT_MIN<<endl;
+    cout<< "Maximum limit of unsigned int data type : "<<UINT_MAX<<endl;
+    cout<< "Maximum limit of long long data type : "<<LLONG_MAX<<endl;
+    cout<< "Minimum limit of long long data type : "<<LLONG_MIN<<endl;
+    cout<< "Maximum limit of unsigned long long data type : "<<ULLONG_MAX<<endl;
+    cout<< "Bits contain in char data type : "<<CHAR_BIT<<endl;
+    cout<< "Maximum limit of char data type : "<<CHAR_MAX<<endl;
+    cout<< "Minimum limit of char data type : "<<CHAR_MIN<<endl;
+    cout<< "Maximum limit of signed char data type : "<<SCHAR_MAX<<endl;
+    cout<< "Minimum limit of signed char data type : "<<SCHAR_MIN<<endl;
+    cout<< "Maximum limit of unsigned char data type : "<<UCHAR_MAX<<endl;
+    cout<< "Minimum limit of short data type : "<<SHRT_MIN<<endl;
+    cout<< "Maximum limit of short data type : "<<SHRT_MAX<<endl;
+    cout<< "Maximum limit of unsigned short data type : "<<USHRT_MAX;
+return 0;
+}
