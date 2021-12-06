75d98bbb6fd3309b0ca18e0105e15d7d43e8f7be
Mahesh Kumbhar <93268828+Maheshkumbhar16@users.noreply.github.com>
2021-12-06 19:29:13 +0530

    volume of a cylinder
    Write a program in C++ to calculate the volume of a cylinder

---
+++ Example_16_volume_of_cylinder.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include<cmath>
+using namespace std;
+int main(){
+   int r=6,h=8;
+   const double pi =3.14;
+   double volumeofcylinder;
+   volumeofcylinder = pi*r*r*h;
+   cout<< "Radius of cylinder is : "<<r<<endl;
+   cout<< "Height of cylinder is : "<<h<<endl;
+   cout<< "Volume of cylinder is : "<<volumeofcylinder;
+return 0;
+}
+
