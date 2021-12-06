6fd9527798e578d45c8330a8d9c3c3c99b1f13ed
Mahesh Kumbhar <93268828+Maheshkumbhar16@users.noreply.github.com>
2021-12-06 19:37:43 +0530

    compute quotient and remainder
    Write a program in C++ to compute quotient and remainder

---
+++ Example_29_quotient_remainder.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x=25, y=3;
+    cout<< "First number is : "<<x<<endl;
+    cout<< "Second number is : "<<y<<endl;
+    cout << "The quotient of the divison is : " <<x/y<<endl;
+    cout<< "The remainder of the division is : "<<x%y;
+return 0;
+}
