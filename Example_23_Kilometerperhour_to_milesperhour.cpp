325be4a7d4ff5aac49258bea52e65f508f715cc3
Mahesh Kumbhar <93268828+Maheshkumbhar16@users.noreply.github.com>
2021-12-06 19:34:14 +0530

    converts kilometers per hour to miles per hour
    Write a program in C++ that converts kilometers per hour to miles per hour

---
+++ Example_23_Kilometerperhour_to_milesperhour.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include<cmath>
+using namespace std;
+int main(){
+//m=k/1.609344
+double k=25,m;
+m=k/1.609344;
+cout << "Speed in kilometer per hour is : "<<k<<endl;
+cout << "Speed in miles in per hour is : "<<m;
+return 0;
+}
