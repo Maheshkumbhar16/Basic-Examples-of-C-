b934398307ca4ef42d304be7ebdcdffbce799383
Mahesh Kumbhar <93268828+Maheshkumbhar16@users.noreply.github.com>
2021-12-06 19:25:53 +0530

    Specified operations
    Write a program in C++ to print the result of the specified operations

---
+++ Example_11_specified_operations.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<< "Result of 1st expression is : "<<10+13<<endl;
+    cout<< "Result of 2nd expression is : "<<15-10<<endl;
+    cout<< "Result of 3rd expression is : "<<3*4<<endl;
+    cout<< "Result of 4th expression is : "<<18/6;
+return 0;
+}
