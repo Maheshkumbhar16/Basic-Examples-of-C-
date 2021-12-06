7d8dc621665c57bab98ab43578a6e19c9780cc11
Mahesh Kumbhar <93268828+Maheshkumbhar16@users.noreply.github.com>
2021-12-06 19:33:38 +0530

    find the third angle of a triangle
    Write a program in C++ to find the third angle of a triangle

---
+++ Example_22_Thrid_angle_of_triangle.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include<cmath>
+using namespace std;
+int main(){
+    int a=30,b=60,c;
+    c=180-a-b;
+    cout <<"First angle of triangle is : "<<a<<endl;
+    cout <<"Second angle of triangle is : "<<b<<endl;
+    cout <<"Third angle of triangle is : "<<c;
+return 0;
+}
