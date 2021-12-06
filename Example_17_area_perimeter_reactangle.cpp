ff3418e2cce5740e9df6b99427d8cc28a5a324ac
Mahesh Kumbhar <93268828+Maheshkumbhar16@users.noreply.github.com>
2021-12-06 19:29:52 +0530

    Area and Perimeter of a Rectangle
    Write a program in C++ to find the Area and Perimeter of a Rectangle

---
+++ Example_17_area_perimeter_reactangle.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+    int l=10,b=15, periofreact, areaofreact;
+    areaofreact = l*b;
+    periofreact = 2*(l+b);
+    cout << "Lenght of reactangle is : " <<l<<endl;
+    cout << "Width of reactangle is : "<<b<<endl;
+    cout << "Perimeter of reactangle is : "<<periofreact<<endl;
+    cout << "Area of reactangle is : "<<areaofreact;
+return 0;
+}
